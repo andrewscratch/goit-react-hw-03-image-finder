7f01c5c96a3a0eb29746f60f9648426512581aa6
andrewscratch <andrew.chudlya@gmail.com>
2023-02-11 21:36:32 +0200

    react hw3
    hw3 done

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -3,20 +3,20 @@ import PropTypes from 'prop-types';
 import { GalleryItem, GalleryImage } from './ImageGalleryItem.styled';
 
 export const Item = ({ webformatURL, largeImageURL, tags, onSelected }) => {
-    return (
-        <GalleryItem>
-            <GalleryImage
-                src={webformatURL}
-                alt={tags}
-                onClick={() => onSelected({ largeImageURL, tags })}
-            />
-        </GalleryItem>
-    );
+  return (
+    <GalleryItem>
+      <GalleryImage
+        src={webformatURL}
+        alt={tags}
+        onClick={() => onSelected({ largeImageURL, tags })}
+      />
+    </GalleryItem>
+  );
 };
 
-Item.propTyepes = {
-    tags: PropTypes.string.isRequired,
-    webformatURL: PropTypes.string.isRequired,
-    largeImageURL: PropTypes.string.isRequired,
-    onSelected: PropTypes.func,
+Item.propTypes = {
+  tags: PropTypes.string.isRequired,
+  webformatURL: PropTypes.string.isRequired,
+  largeImageURL: PropTypes.string.isRequired,
+  onSelected: PropTypes.func,
 };
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -6,37 +6,36 @@ import { Backdrop, ModalWrapper } from './Modal.styled';
 const modalRoot = document.querySelector('#modal-root');
 
 export class Modal extends Component {
-    componentDidMount() {
-        window.addEventListener('keydown', this.handleKeyDown);
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = evt => {
+    if (evt.code === 'Escape') {
+      this.props.onCloseByEscape();
     }
+  };
 
-    componentWillUnmount() {
-        window.removeEventListener('keydown', this.handleKeyDown);
-    }
-
-    handleKeyDown = evt => {
-        if (evt.code === 'Escape') {
-            this.props.onCloseByEscape();
-        }
-    };
-
-    render() {
-        const { largeImg, tags, onCloseByClick } = this.props;
-        return createPortal(
-            <Backdrop id="backdrop" onClick={onCloseByClick}>
-                <ModalWrapper>
-                    <img src={largeImg} alt={tags} />
+  render() {
+    const { largeImg, tags, onCloseByClick } = this.props;
+    return createPortal(
+      <Backdrop id="backdrop" onClick={onCloseByClick}>
+        <ModalWrapper>
+          <img src={largeImg} alt={tags} />
         </ModalWrapper>
       </Backdrop>,
       modalRoot
     );
   }
 }
-             
 
 Modal.propTypes = {
-    largeImg: PropTypes.string.isRequired,
-    tags: PropTypes.string.isRequired,
-    onCloseByEscape: PropTypes.func,
-    onCloseByClick: PropTypes.func,
+  largeImg: PropTypes.string.isRequired,
+  tags: PropTypes.string.isRequired,
+  onCloseByEscape: PropTypes.func,
+  onCloseByClick: PropTypes.func,
 };
